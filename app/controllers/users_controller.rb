d69157f579e82787690366b603c0638297fa63a9
billhimmelsbach <whimmels@gmail.com>
2016-08-21 13:03:24 -0700

    changes the default user image behavior from the front end to the backend using the create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
   def create
 
         @user = User.new(user_params)
+        if @user.image_url == nil
+          @user.image_url = "http://i.imgur.com/ixpF82Y.png"
+        end
 
         if @user.save
           login(@user)
