493e73ff272cf70558157280523de88b5e9c8071
Ted Anyansi <tanyansi@gmail.com>
2016-08-20 17:26:20 -0700

    working on validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,28 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    login(@user)
-    redirect_to @user
+    # user = User.create(user_params)
+        #
+        # p user.errors.messages
+        # render plain: User.create(user_params).errors.full_messages.inspect
+        user = User.new(user_params)
+        user = user.valid?
+        if user.save
+          flash[:notice] =  "yo"
+          # flash[:notice] = "Sorry darling, but your desired email has already been claimed. Please choose a different one :P"
+          redirect_to root_path
+        else
+          flash[:notice] = user.errors.full_messages
+          redirect_to root_path
+        end
+      # if @user
+        # @user =  User.create(user_params)
+        #   login(@user)
+        #   flash[:notice] = "Successfully signed up!"
+        #   redirect_to root_path
+      # else
+      #   flash[:error] = @user.errors.full_messages
+      # end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :posts
   has_many :cities, :through => :posts
+  validates :email, uniqueness: true
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
