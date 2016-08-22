8d2f26f6a6b4606be561753414666d0b26e79562
Ted Anyansi <tanyansi@gmail.com>
2016-08-21 23:00:17 -0700

    testing routes

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,12 @@
 class City < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   has_many :posts
   has_many :users, :through => :posts
+
+  attr_accessor :content, :title
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819202015) do
+ActiveRecord::Schema.define(version: 20160822052751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160819202015) do
     t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
   create_table "city_posts", force: :cascade do |t|
